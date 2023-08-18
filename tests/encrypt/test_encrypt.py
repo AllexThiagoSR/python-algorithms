243f16f46b939fb9290880b5f7321cc54da2111c
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-08-18 19:39:33 -0300

    Tenta implementar alguns testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
 
 
-def test_encrypt_message():
-    pass
+def test_encrypt_message_odd():
+    assert encrypt_message('feijão', 3) == 'ief_oãj'
+
+
+def test_encrypt_message_even():
+    assert encrypt_message('feijão', 4) == 'oã_jief'
+
+
+def test_encrypt_message_with_invalid_index():
+    assert encrypt_message('feijão', -1) == 'oãjief'
