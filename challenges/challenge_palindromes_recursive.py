fd34c11b442973bcf4440b4244c9fce77c122ce6
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-08-18 19:50:43 -0300

    Requisito 3: Concluído

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (word == ''):
+        return False
+    if low_index == high_index or low_index > high_index:
+        return True
+    return (
+        word[low_index] == word[high_index]
+        and
+        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    )
+
+
+print(is_palindrome_recursive('arara', 0, len('arara') - 1))
