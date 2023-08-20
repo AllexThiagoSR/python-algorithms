b37da73d1bb75d3abc5e22c2ceaeb73fcb1f42ec
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-08-20 17:55:02 -0300

    Requisito 5: Concluído

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums: list[int]):
+    sorted_nums = sorted(nums)
+
+    try:
+        for index in range(1, len(sorted_nums)):
+            if sorted_nums[index - 1] == sorted_nums[index] and not (
+                sorted_nums[0] < 1 or sorted_nums[index] < 1
+            ):
+                return sorted_nums[index]
+        return False
+
+    except TypeError:
+        return False
