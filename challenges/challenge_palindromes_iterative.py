a898521a26997f6c4d8563e1330205d651c9817a
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-08-20 18:11:21 -0300

    Requisito 6: Concluído

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str):
+    reversed_word = word.replace('', ' ').split()
+    reversed_word.reverse()
+    if len(word) < 1:
+        return False
+    return "".join(reversed_word).lower() == word.lower()
+
+
+if __name__ == '__main__':
+    print(is_palindrome_iterative(''))
